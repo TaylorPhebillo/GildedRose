32f48c80c74f21cc23af62eb53b78ea125a4ac7f
taylor <taylor.phebillo@gmail.com>
2022-01-27 13:34:20 -0600

    Mostly working- tickets not updating correctly.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,12 +1,58 @@
 # -*- coding: utf-8 -*-
+from dataclasses import dataclass
+from collections import defaultdict
+@dataclass
+class Item:
+    name: str
+    sell_in: int
+    quality: int
 
+start_of_time = -float('inf')
+
+@dataclass
+class DailyItemChanges(object):
+    quality_limits = [0,50]
+    quality_delta = [(start_of_time, -1), (0,-2)]
+
+item_changes = defaultdict(DailyItemChanges)
+
+brie = DailyItemChanges()
+brie.quality_delta = [(start_of_time, 1)]
+item_changes["Aged Brie"] = brie
+
+sulfuras = DailyItemChanges()
+sulfuras.quality_limits = [80,80]
+item_changes["Sulfuras, Hand of Ragnaros"] = sulfuras
+
+backstage = DailyItemChanges()
+backstage.quality_delta = [(start_of_time, 1), (-10, 2), (-5, 3), (0, -float('inf'))]
+item_changes["Backstage passes to a TAFKAL80ETC concert"] = backstage
+
+conjured = DailyItemChanges()
+conjured.quality_delta = [(start_of_time, -2)]
+item_changes["conjured"] = conjured
+
+def update_item_quality(item):
+    item_change = item_changes[item.name]
+    print(item_change)
+    item.sell_in -= 1
+    quality_change = next((x[1] for x in reversed(item_change.quality_delta) if x[0] > item.sell_in), item_change.quality_delta[0][1])
+    print(f"Quality change for {item} = {quality_change} via {item_change}, in particular {item_change.quality_delta}")
+    item.quality += quality_change
+    item.quality = max(min(item.quality, item_change.quality_limits[1]), item_change.quality_limits[0])
+    print(f"Updated to {item}")
+    return item
+
+@dataclass
 class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
+    items: [Item]
 
     def update_quality(self):
+        self.items = [update_item_quality(item) for item in self.items]
+
+    def update_quality_old(self):
         for item in self.items:
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -35,12 +81,3 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,28 @@
-# -*- coding: utf-8 -*-
+#!usr/local/bin/python3 
+# # -*- coding: utf-8 -*-
 import unittest
 
 from gilded_rose import Item, GildedRose
+from copy import deepcopy
 
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-
+        items = [
+            #Item("foo", 0, 3),
+            #Item("Aged Brie", 100, 10),
+            Item("Backstage passes to a TAFKAL80ETC concert", 20, 12),
+            #Item("Sulfuras, Hand of Ragnaros", 80, 100)
+            ]
+        for _ in range(500):
+            gilded_rose_new = GildedRose(deepcopy(items))
+            gilded_rose_new.update_quality()
+            gilded_rose_old = GildedRose(items)
+            gilded_rose_old.update_quality_old()
+            self.assertEqual(gilded_rose_new, gilded_rose_old)
+            self.assertEqual(gilded_rose_new.items, gilded_rose_old.items)
+            for i in range(len(gilded_rose_old.items)):
+                self.assertEqual(gilded_rose_old.items[i], gilded_rose_new.items[i])
         
 if __name__ == '__main__':
     unittest.main()
