cfcba453dcdd3ba84d927f314f1381e6eab18058
taylor <taylor.phebillo@gmail.com>
2022-01-27 14:13:51 -0600

    Complete

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -2,22 +2,26 @@
 from dataclasses import dataclass
 from collections import defaultdict
 @dataclass
+# Yeah, we angered the goblin, but it's API compatible and goblins are dumb, so we're probably fine.
 class Item:
     name: str
     sell_in: int
     quality: int
 
-start_of_time = -float('inf')
+start_of_time = float('inf')
 
 @dataclass
 class DailyItemChanges(object):
     quality_limits = [0,50]
-    quality_delta = [(start_of_time, -1), (0,-2)]
+    # TODO: Make quality delta entries a more sensible object/named tuple, instead of magic tuple
+    # Starting at this many days until the sell_in date, update the quality by this much
+    # format (sell_in limit, quality change)
+    quality_delta = [(0, -2), (start_of_time, -1)]
 
 item_changes = defaultdict(DailyItemChanges)
 
 brie = DailyItemChanges()
-brie.quality_delta = [(start_of_time, 1)]
+brie.quality_delta = [(0, 2), (start_of_time, 1)]
 item_changes["Aged Brie"] = brie
 
 sulfuras = DailyItemChanges()
@@ -25,7 +29,7 @@ sulfuras.quality_limits = [80,80]
 item_changes["Sulfuras, Hand of Ragnaros"] = sulfuras
 
 backstage = DailyItemChanges()
-backstage.quality_delta = [(start_of_time, 1), (-10, 2), (-5, 3), (0, -float('inf'))]
+backstage.quality_delta = [(0, -float('inf')), (5, 3), (10, 2), (start_of_time, 1)]
 item_changes["Backstage passes to a TAFKAL80ETC concert"] = backstage
 
 conjured = DailyItemChanges()
@@ -34,13 +38,10 @@ item_changes["conjured"] = conjured
 
 def update_item_quality(item):
     item_change = item_changes[item.name]
-    print(item_change)
     item.sell_in -= 1
-    quality_change = next((x[1] for x in reversed(item_change.quality_delta) if x[0] > item.sell_in), item_change.quality_delta[0][1])
-    print(f"Quality change for {item} = {quality_change} via {item_change}, in particular {item_change.quality_delta}")
+    quality_change = next((x[1] for x in item_change.quality_delta if x[0] > item.sell_in), item_change.quality_delta[0][1])
     item.quality += quality_change
     item.quality = max(min(item.quality, item_change.quality_limits[1]), item_change.quality_limits[0])
-    print(f"Updated to {item}")
     return item
 
 @dataclass
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -7,17 +7,44 @@ from copy import deepcopy
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+    def test_conjured(self):
         items = [
-            #Item("foo", 0, 3),
-            #Item("Aged Brie", 100, 10),
-            Item("Backstage passes to a TAFKAL80ETC concert", 20, 12),
-            #Item("Sulfuras, Hand of Ragnaros", 80, 100)
+            Item("conjured", 1000, 50),
+            Item("conjured", 100, 10)
             ]
-        for _ in range(500):
-            gilded_rose_new = GildedRose(deepcopy(items))
+        gilded_rose_new = GildedRose(deepcopy(items))
+        for i in range(1, 500):
             gilded_rose_new.update_quality()
-            gilded_rose_old = GildedRose(items)
+            self.assertEqual(gilded_rose_new.items[0].quality, max(50 - 2 * i, 0))
+            self.assertEqual(gilded_rose_new.items[1].quality, max(10 - 2 * i, 0))
+
+    def test_compare_sulfuras(self):
+        # The new implementation changes the sell_in date for Sulfuras, the old does not- only the quality should match.
+        items = [
+            Item("Sulfuras, Hand of Ragnaros", 1000, 80),
+            Item("Sulfuras, Hand of Ragnaros", 100, 80)
+            ]
+        gilded_rose_new = GildedRose(deepcopy(items))
+        gilded_rose_old = GildedRose(items)
+        for _ in range(500):
+            gilded_rose_new.update_quality()
+            gilded_rose_old.update_quality_old()
+            self.assertEqual(len(gilded_rose_new.items), len(gilded_rose_old.items))
+            for i in range(len(gilded_rose_old.items)):
+                self.assertEqual(gilded_rose_old.items[i].quality, gilded_rose_new.items[i].quality)
+
+    def test_compare_with_old(self):
+        items = [
+            Item("foo", 100, 3),
+            Item("foo", 0, 3),
+            Item("Aged Brie", 100, 10),
+            Item("Aged Brie", 3, 1),
+            Item("Backstage passes to a TAFKAL80ETC concert", 20, 12),
+            ]
+        gilded_rose_new = GildedRose(deepcopy(items))
+        gilded_rose_old = GildedRose(items)
+        for _ in range(500):
+            gilded_rose_new.update_quality()
             gilded_rose_old.update_quality_old()
             self.assertEqual(gilded_rose_new, gilded_rose_old)
             self.assertEqual(gilded_rose_new.items, gilded_rose_old.items)
